dc112e5cebf82ba5d7fe8d394027e07be56fccc0
rio oktaviansyah <riooktaviansyah36@gmail.com>
2024-02-11 18:24:18 +0700

    Actions in livewire

---
+++ app/Livewire/CreatePoll.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreatePoll extends Component
+{
+    public $title;
+    public $options = ['First'];
+    public function render()
+    {
+        return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+}
