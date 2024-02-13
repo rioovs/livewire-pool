9a4b879f1c23105f602c61b3b7c867fae7ef70fb
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2024-02-13 18:57:19 +0700

    add vote using Data Model Binding

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use Livewire\Component;
 use Livewire\Attributes\On;
 
@@ -14,5 +15,19 @@ class Polls extends Component
         $polls = \App\Models\Poll::with('options.votes')->latest()->get();
 
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+
+    // Method 1
+    // public function vote($optionId)
+    // {
+    //     $option = \App\Models\Option::findOrFail($optionId);
+    //     $option->votes()->create();
+    // }
+
+    // Method 2 Using Data Model Binding
+    function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
